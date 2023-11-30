d5699eb89c9bfdd397c35f84b1900a4a32784b11
Jaensson <elias.jaensson@gmail.com>
2023-11-30 19:38:20 +0100

    Add read_file helper function

---
+++ helpers.py
@@ -0,0 +1,14 @@
+from typing import List
+from enum import Enum
+
+
+class File(Enum):
+    READ = "r"
+
+
+def read_file(file: str) -> List[str]:
+    file = open(file, File.READ.value, encoding="UTF-8")
+    lines = file.read()
+    file.close()
+
+    return lines
