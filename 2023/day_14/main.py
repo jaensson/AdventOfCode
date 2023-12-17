539fd0e6229da1116c04d0132072be5e838b0f02
Jaensson <elias.jaensson@gmail.com>
2023-12-18 00:56:52 +0100

    Complete Day 14 2023

--- 2023/day_14/main.py
+++ 2023/day_14/main.py
@@ -77,71 +77,169 @@ def part1(lines):
     return load
 
 
-def part2(lines):
-    CYCLES = 3
-    rounded_stones, cubed_stones = get_stones(lines)
+def tilt_up(rounded_stones, horizontal_cubed_stones):
+    cubed_stones = dict()
 
-    directions = ["north", "west", "south", "east"]
+    for i, rounded_stone in enumerate(rounded_stones):
+        x, y = rounded_stone
+        current_cubed_stone = len(horizontal_cubed_stones[x]) - 1
+        while (
+            current_cubed_stone >= 0
+            and y <= horizontal_cubed_stones[x][current_cubed_stone][1]
+        ):
+            current_cubed_stone -= 1
+
+        rest_stone = (
+            (x, -1)
+            if current_cubed_stone < 0
+            else horizontal_cubed_stones[x][current_cubed_stone]
+        )
+
+        if rest_stone not in cubed_stones:
+            cubed_stones[rest_stone] = 0
+
+        cubed_stones[rest_stone] += 1
+        rounded_stones[i] = (x, rest_stone[1] + cubed_stones[rest_stone])
+
+
+def tilt_left(rounded_stones, vertical_cubed_stones):
+    cubed_stones = dict()
+
+    for i, rounded_stone in enumerate(rounded_stones):
+        x, y = rounded_stone
+        current_cubed_stone = len(vertical_cubed_stones[y]) - 1
+        while (
+            current_cubed_stone >= 0
+            and x <= vertical_cubed_stones[y][current_cubed_stone][0]
+        ):
+            current_cubed_stone -= 1
+
+        rest_stone = (
+            (-1, y)
+            if current_cubed_stone < 0
+            else vertical_cubed_stones[y][current_cubed_stone]
+        )
+
+        if rest_stone not in cubed_stones:
+            cubed_stones[rest_stone] = 0
+
+        cubed_stones[rest_stone] += 1
+        rounded_stones[i] = (rest_stone[0] + cubed_stones[rest_stone], y)
+
+
+def tilt_down(rounded_stones, horizontal_cubed_stones, height):
+    cubed_stones = dict()
+
+    for i, rounded_stone in enumerate(rounded_stones):
+        x, y = rounded_stone
+        current_cubed_stone = len(horizontal_cubed_stones[x]) - 1
+        while (
+            current_cubed_stone >= 0
+            and y
+            >= horizontal_cubed_stones[x][
+                len(horizontal_cubed_stones[x]) - 1 - current_cubed_stone
+            ][1]
+        ):
+            current_cubed_stone -= 1
+
+        rest_stone = (
+            (x, height)
+            if current_cubed_stone < 0
+            else horizontal_cubed_stones[x][
+                len(horizontal_cubed_stones[x]) - 1 - current_cubed_stone
+            ]
+        )
+
+        if rest_stone not in cubed_stones:
+            cubed_stones[rest_stone] = 0
+
+        cubed_stones[rest_stone] += 1
+        rounded_stones[i] = (x, rest_stone[1] - cubed_stones[rest_stone])
+
+
+def tilt_right(rounded_stones, vertical_cubed_stones, width):
+    cubed_stones = dict()
+
+    for i, rounded_stone in enumerate(rounded_stones):
+        x, y = rounded_stone
+        current_cubed_stone = len(vertical_cubed_stones[y]) - 1
+        while (
+            current_cubed_stone >= 0
+            and x
+            >= vertical_cubed_stones[y][
+                len(vertical_cubed_stones[y]) - 1 - current_cubed_stone
+            ][0]
+        ):
+            current_cubed_stone -= 1
+
+        rest_stone = (
+            (width, y)
+            if current_cubed_stone < 0
+            else vertical_cubed_stones[y][
+                len(vertical_cubed_stones[y]) - 1 - current_cubed_stone
+            ]
+        )
+
+        if rest_stone not in cubed_stones:
+            cubed_stones[rest_stone] = 0
+
+        cubed_stones[rest_stone] += 1
+        rounded_stones[i] = (rest_stone[0] - cubed_stones[rest_stone], y)
+
+
+def part2(lines):
+    seen_cycles = dict()
+    CYCLES = 1000000000
+    rounded_stones, cubed_stones = get_stones(lines)
+    unique_cycle = 0
+
+    repeats = set()
+
+    horizontal_cubed_stones = [[] for _ in range(len(lines[0]))]
+    vertical_cubed_stones = [[] for _ in range(len(lines))]
+
+    for cubed_stone in cubed_stones:
+        x, y = cubed_stone
+        horizontal_cubed_stones[x].append(cubed_stone)
+        horizontal_cubed_stones[x].sort()
+        vertical_cubed_stones[y].append(cubed_stone)
+        vertical_cubed_stones[y].sort()
 
     for cycle in range(CYCLES):
-        if cycle % 1000000 == 0:
-            print(cycle)
-        for direction in directions:
-            match direction:
-                case "north":
-                    rounded_stones.sort(key=lambda coordinates: coordinates[1])
-                    for i, rounded_stone in enumerate(rounded_stones):
-                        x, y = rounded_stone
-                        while (
-                            y - 1 >= 0
-                            and (x, y - 1) not in rounded_stones
-                            and (x, y - 1) not in cubed_stones
-                        ):
-                            y -= 1
+        rounded_stones.sort()
+        before = str(rounded_stones)
+        if before in seen_cycles:
+            if seen_cycles[before][1] in repeats:
+                break
 
-                        rounded_stones[i] = (x, y)
-                case "west":
-                    rounded_stones.sort(key=lambda coordinates: coordinates[0])
-                    for i, rounded_stone in enumerate(rounded_stones):
-                        x, y = rounded_stone
-                        while (
-                            x - 1 >= 0
-                            and (x - 1, y) not in rounded_stones
-                            and (x - 1, y) not in cubed_stones
-                        ):
-                            x -= 1
+            repeats.add(seen_cycles[before][1])
+            rounded_stones = seen_cycles[before][0]
+            continue
 
-                        rounded_stones[i] = (x, y)
-                case "south":
-                    rounded_stones.sort(
-                        key=lambda coordinates: coordinates[1], reverse=True
-                    )
-                    for i, rounded_stone in enumerate(rounded_stones):
-                        x, y = rounded_stone
-                        while (
-                            y + 1 < len(lines)
-                            and (x, y + 1) not in rounded_stones
-                            and (x, y + 1) not in cubed_stones
-                        ):
-                            y += 1
+        tilt_up(rounded_stones, horizontal_cubed_stones)
+        tilt_left(rounded_stones, vertical_cubed_stones)
+        tilt_down(rounded_stones, horizontal_cubed_stones, len(lines))
+        tilt_right(rounded_stones, vertical_cubed_stones, len(lines[0]))
 
-                        rounded_stones[i] = (x, y)
-                case "east":
-                    rounded_stones.sort(
-                        key=lambda coordinates: coordinates[0], reverse=True
-                    )
-                    for i, rounded_stone in enumerate(rounded_stones):
-                        x, y = rounded_stone
-                        while (
-                            x + 1 < len(lines[0])
-                            and (x + 1, y) not in rounded_stones
-                            and (x + 1, y) not in cubed_stones
-                        ):
-                            x += 1
+        seen_cycles[before] = (rounded_stones[::], unique_cycle)
+        unique_cycle += 1
 
-                        rounded_stones[i] = (x, y)
+    seen_cycles = list(seen_cycles.values())
+    seen_cycles.sort(key=lambda cycles: cycles[1])
 
-    draw_stones(lines, rounded_stones, cubed_stones)
+    new_seen_cycles = []
+    for seen_cycle in seen_cycles:
+        if seen_cycle[1] in repeats:
+            new_seen_cycles.append(seen_cycle[0])
+
+    loads = []
+    for elem in new_seen_cycles:
+        load = 0
+        for x, y in elem:
+            load += len(lines) - y
+        loads.append(load)
+
+    return loads[(CYCLES - 1 - min(repeats)) % len(new_seen_cycles)]
 
 
 if __name__ == "__main__":
