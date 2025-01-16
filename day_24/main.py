2b089b922dcf1037a791f2d132d861f1f37fa826
jaensson <elias.jaensson@gmail.com>
2025-01-16 14:23:53 +0100

    Remove debug prints

--- day_24/main.py
+++ day_24/main.py
@@ -109,26 +109,6 @@ def part2(registers, instructions):
             ):
                 return instruction
 
-    def get_corresponding_instructions(number):
-        if number == 0:
-            valid = set()
-            for instruction in instructions:
-                used_registers = set()
-                used_registers.add(instruction["left"])
-                used_registers.add(instruction["right"])
-
-                if (
-                    f"x{number:02}" in used_registers
-                    and f"y{number:02}" in used_registers
-                ):
-                    valid.add(tuple(instruction.items()))
-
-            result = []
-            for val in valid:
-                result.append(dict(val))
-
-            return result
-
     def get_wrong_outputs(instructions, registers, number, wrong, carry=None):
         if f"x{number:02}" not in registers:
             return
@@ -145,8 +125,6 @@ def part2(registers, instructions):
                 instructions, registers, number + 1, wrong, input_and["result"]
             )
 
-        # print(number, carry)
-
         input_and = get_specific_instruction(
             instructions, set([f"x{number:02}", f"y{number:02}"]), "AND"
         )
@@ -160,13 +138,7 @@ def part2(registers, instructions):
             instructions, set([carry, input_xor["result"]]), "AND"
         )
 
-        # print("input and:", input_and)
-        # print("input xor:", input_xor)
-        # print("sum:", sum)
-        # print("tmp:", tmp)
-
         if sum is None and tmp is None:
-            print("either the in carry is wrong or the value from input_xor")
             sum = get_instruction(
                 instructions, set([carry, input_xor["result"]]), "XOR"
             )
@@ -184,44 +156,29 @@ def part2(registers, instructions):
 
             if carry in used:
                 used.remove(carry)
-                expected = list(used)[0]
-                print(
-                    f"wrong input xor register should be {expected} but got",
-                    input_xor["result"],
-                )
                 wrong.add(input_xor["result"])
-                wrong.add(expected)
+                wrong.add(list(used)[0])
 
             if input_xor["result"] in used:
                 used.remove(input_xor["result"])
-                expected = list(used)[0]
-                print(
-                    f"wrong in carry register should be {expected} but got {carry}"
-                )
                 wrong.add(carry)
-                wrong.add(expected)
+                wrong.add(list(used)[0])
 
             out_carry = get_instruction(
                 instructions, set([input_and["result"], tmp["result"]]), "OR"
             )
-            # print("out carry:", out_carry)
 
             return get_wrong_outputs(
                 instructions, registers, number + 1, wrong, out_carry["result"]
             )
 
         if f"z{number:02}" != sum["result"]:
-            print(
-                f"wrong sum register should be z{number:02} but got",
-                sum["result"],
-            )
             wrong.add(sum["result"])
             wrong.add(f"z{number:02}")
 
             out_carry = get_instruction(
                 instructions, set([input_and["result"], tmp["result"]]), "OR"
             )
-            # print("out carry:", out_carry)
 
             return get_wrong_outputs(
                 instructions, registers, number + 1, wrong, out_carry["result"]
@@ -229,8 +186,6 @@ def part2(registers, instructions):
         out_carry = get_specific_instruction(
             instructions, set([input_and["result"], tmp["result"]]), "OR"
         )
-
-        # print("out carry:", out_carry)
 
         get_wrong_outputs(
             instructions, registers, number + 1, wrong, out_carry["result"]
