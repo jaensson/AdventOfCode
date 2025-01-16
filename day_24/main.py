be3cdb897874a40a0b67326a505ec3f7bd2b9f55
jaensson <elias.jaensson@gmail.com>
2025-01-16 14:22:12 +0100

    Complete part 2 day 24

--- day_24/main.py
+++ day_24/main.py
@@ -91,86 +91,153 @@ def part1(registers, instructions):
 
 
 def part2(registers, instructions):
-    def full_adder(instructions, number):
-        def get_input_xor(number):
-            for instruction in instructions:
-                if (
-                    f"{number:02}" in instruction["left"]
-                    and f"{number:02}" in instruction["right"]
-                    and instruction["operand"] == "XOR"
-                ):
-                    return instruction["result"]
-
-            return ""
-
-        def get_input_and(number):
-            for instruction in instructions:
-                if (
-                    f"{number:02}" in instruction["left"]
-                    and f"{number:02}" in instruction["right"]
-                    and instruction["operand"] == "AND"
-                ):
-                    return instruction["result"]
-            return ""
-
-        def get_carry(number):
-            if number == 0:
-                return get_input_and(number)
-
-            input_and = get_input_and(number)
-            input_xor = get_input_xor(number)
-
-            for instruction in instructions:
-                keys = ["left", "right", "result"]
-                for key in keys:
-                    if (
-                        f"{number:02}" in instruction[key]
-                        or input_xor in instruction[key]
-                        or input_and in instruction[key]
-                    ) and instruction["operand"] == "OR":
-                        return instruction["result"]
-            return ""
-
-        def get_temp(valid, carry, input_xor):
-            for val in valid:
-                val = dict(val)
-                used_registers = set()
-                used_registers.add(val["left"])
-                used_registers.add(val["right"])
-                if (
-                    val["operand"] == "AND"
-                    and val["left"] in used_registers
-                    and val["right"] in used_registers
-                ):
-                    return val["result"]
-            return ""
-
-        valid = set()
-        carry = get_carry(number - 1)
-        input_xor = get_input_xor(number)
-        input_and = get_input_and(number)
-
-        print(input_xor, input_and, number)
-
+    def get_specific_instruction(instructions, registers, operand):
         for instruction in instructions:
-            keys = ["left", "right", "result"]
-            for key in keys:
+            used = set()
+            used.add(instruction["left"])
+            used.add(instruction["right"])
+
+            if used == registers and instruction["operand"] == operand:
+                return instruction
+
+    def get_instruction(instructions, registers, operand):
+        for instruction in instructions:
+            used = set([instruction["left"], instruction["right"]])
+            if (
+                len(used.intersection(registers)) != 0
+                and instruction["operand"] == operand
+            ):
+                return instruction
+
+    def get_corresponding_instructions(number):
+        if number == 0:
+            valid = set()
+            for instruction in instructions:
+                used_registers = set()
+                used_registers.add(instruction["left"])
+                used_registers.add(instruction["right"])
+
                 if (
-                    f"{number:02}" in instruction[key]
-                    or input_xor in instruction[key]
-                    or input_and in instruction[key]
+                    f"x{number:02}" in used_registers
+                    and f"y{number:02}" in used_registers
                 ):
                     valid.add(tuple(instruction.items()))
 
-        for val in valid:
-            val = dict(val)
-            print(val)
+            result = []
+            for val in valid:
+                result.append(dict(val))
 
-        # tmp = get_temp(valid, carry, input_xor)
+            return result
 
-    start = 1
-    while f"x{start:02}" in registers:
-        # result = full_adder(instructions, start)
-        start += 1
+    def get_wrong_outputs(instructions, registers, number, wrong, carry=None):
+        if f"x{number:02}" not in registers:
+            return
 
-    result = full_adder(instructions, 25)
+        if number == 0:
+            input_and = get_specific_instruction(
+                instructions, set([f"x{0:02}", f"y{0:02}"]), "AND"
+            )
+            input_xor = get_specific_instruction(
+                instructions, set([f"x{0:02}", f"y{0:02}"]), "XOR"
+            )
+
+            return get_wrong_outputs(
+                instructions, registers, number + 1, wrong, input_and["result"]
+            )
+
+        # print(number, carry)
+
+        input_and = get_specific_instruction(
+            instructions, set([f"x{number:02}", f"y{number:02}"]), "AND"
+        )
+        input_xor = get_specific_instruction(
+            instructions, set([f"x{number:02}", f"y{number:02}"]), "XOR"
+        )
+        sum = get_specific_instruction(
+            instructions, set([carry, input_xor["result"]]), "XOR"
+        )
+        tmp = get_specific_instruction(
+            instructions, set([carry, input_xor["result"]]), "AND"
+        )
+
+        # print("input and:", input_and)
+        # print("input xor:", input_xor)
+        # print("sum:", sum)
+        # print("tmp:", tmp)
+
+        if sum is None and tmp is None:
+            print("either the in carry is wrong or the value from input_xor")
+            sum = get_instruction(
+                instructions, set([carry, input_xor["result"]]), "XOR"
+            )
+            tmp = get_instruction(
+                instructions, set([carry, input_xor["result"]]), "AND"
+            )
+
+            used_sum = set([sum["left"], sum["right"]])
+            used_tmp = set([tmp["left"], tmp["right"]])
+            if used_tmp != used_sum:
+                print("this should not happen?")
+                print(used_sum, used_tmp)
+
+            used = used_sum
+
+            if carry in used:
+                used.remove(carry)
+                expected = list(used)[0]
+                print(
+                    f"wrong input xor register should be {expected} but got",
+                    input_xor["result"],
+                )
+                wrong.add(input_xor["result"])
+                wrong.add(expected)
+
+            if input_xor["result"] in used:
+                used.remove(input_xor["result"])
+                expected = list(used)[0]
+                print(
+                    f"wrong in carry register should be {expected} but got {carry}"
+                )
+                wrong.add(carry)
+                wrong.add(expected)
+
+            out_carry = get_instruction(
+                instructions, set([input_and["result"], tmp["result"]]), "OR"
+            )
+            # print("out carry:", out_carry)
+
+            return get_wrong_outputs(
+                instructions, registers, number + 1, wrong, out_carry["result"]
+            )
+
+        if f"z{number:02}" != sum["result"]:
+            print(
+                f"wrong sum register should be z{number:02} but got",
+                sum["result"],
+            )
+            wrong.add(sum["result"])
+            wrong.add(f"z{number:02}")
+
+            out_carry = get_instruction(
+                instructions, set([input_and["result"], tmp["result"]]), "OR"
+            )
+            # print("out carry:", out_carry)
+
+            return get_wrong_outputs(
+                instructions, registers, number + 1, wrong, out_carry["result"]
+            )
+        out_carry = get_specific_instruction(
+            instructions, set([input_and["result"], tmp["result"]]), "OR"
+        )
+
+        # print("out carry:", out_carry)
+
+        get_wrong_outputs(
+            instructions, registers, number + 1, wrong, out_carry["result"]
+        )
+
+    wrong = set()
+
+    get_wrong_outputs(instructions, registers, 0, wrong)
+
+    return ",".join(sorted(list(wrong)))
