866c6f297a046e1c5820a88d9ed9c0e7e4f02b45
jaensson <elias.jaensson@gmail.com>
2025-12-12 01:06:32 +0100

    Test corners part 2 day 9

--- day_9/main.py
+++ day_9/main.py
@@ -1,6 +1,7 @@
 import os
 from lib.helpers import read_file
 from enum import IntEnum
+from typing import Self
 
 
 def main():
@@ -35,14 +36,13 @@ def part1(tiles):
     return max_area
 
 
-def part2(tiles):
-    class Direction(IntEnum):
-        LEFT = 1
-        RIGHT = 2
-        DOWN = 3
-        UP = 4
+class Direction(IntEnum):
+    LEFT = 1
+    RIGHT = 2
+    DOWN = 3
+    UP = 4
 
-    def direction(first, second):
+    def direction(first, second) -> Self:
         if first[0] < second[0]:
             return Direction.RIGHT
         if first[0] > second[0]:
@@ -52,71 +52,69 @@ def part2(tiles):
         if first[1] > second[1]:
             return Direction.UP
 
-    def ascending(first, second):
-        return (first, second) if first < second else (second, first)
 
-    def is_inside(rectangles, corner):
-        for first, second in rectangles:
-            min_x, max_x = ascending(first[0], second[0])
-            min_y, max_y = ascending(first[1], second[1])
+class Corner(IntEnum):
+    TOP_LEFT = 1
+    TOP_RIGHT = 2
+    BOTTOM_RIGHT = 3
+    BOTTOM_LEFT = 4
 
-            if min_x <= corner[0] <= max_x and min_y <= corner[1] <= max_y:
-                return True
+    def corner(first: Direction, second: Direction) -> Self:
+        corner_mapping = {
+            (Direction.UP, Direction.RIGHT): Corner.TOP_LEFT,
+            (Direction.LEFT, Direction.DOWN): Corner.TOP_LEFT,
+            (Direction.UP, Direction.LEFT): Corner.TOP_RIGHT,
+            (Direction.RIGHT, Direction.DOWN): Corner.TOP_RIGHT,
+            (Direction.DOWN, Direction.LEFT): Corner.BOTTOM_RIGHT,
+            (Direction.RIGHT, Direction.UP): Corner.BOTTOM_RIGHT,
+            (Direction.DOWN, Direction.RIGHT): Corner.BOTTOM_LEFT,
+            (Direction.LEFT, Direction.UP): Corner.BOTTOM_LEFT,
+        }
+        return corner_mapping[(first, second)]
 
-        return False
 
-    red_tiles = set(tiles)
-    valid_rectangles = set()
-    tiles.append(tiles[0])
-    tiles.append(tiles[1])
-    for i in range(len(tiles) - 2):
-        first, middle, second = tiles[i], tiles[i + 1], tiles[i + 2]
+def part2(tiles):
+    number_of_tiles = len(tiles)
 
-        min_x, max_x = ascending(first[0], second[0])
-        min_y, max_y = ascending(first[1], second[1])
-
-        first_step = direction(first, middle)
-        second_step = direction(middle, second)
-
-        is_part_of_figure = (
-            (first_step == Direction.UP and second_step == Direction.RIGHT)
-            or (first_step == Direction.DOWN and second_step == Direction.LEFT)
-            or (first_step == Direction.LEFT and second_step == Direction.UP)
-            or (first_step == Direction.RIGHT and second_step == Direction.DOWN)
+    corners = []
+    for i in range(number_of_tiles):
+        prev, current, next = (
+            tiles[(i - 1) % number_of_tiles],
+            tiles[i],
+            tiles[(i + 1) % number_of_tiles],
         )
-        if not is_part_of_figure:
-            continue
 
-        is_rectangle = True
-        for x, y in red_tiles:
-            if min_x < x < max_x and min_y < y < max_y:
-                is_rectangle = False
+        prev_direction = Direction.direction(prev, current)
+        next_direction = Direction.direction(current, next)
+        corner = Corner.corner(prev_direction, next_direction)
+        corners.append((current, corner))
+        # print(
+        #     prev, current, next, prev_direction.name, next_direction.name, corner.name
+        # )
 
-        if not is_rectangle:
-            continue
+    # print(len(corners))
 
-        valid_rectangles.add((first, second))
+    valid_corner = {
+        Corner.TOP_LEFT: (
+            lambda first, second: first[0] <= second[0] and first[1] <= second[1]
+        ),
+        Corner.TOP_RIGHT: (
+            lambda first, second: first[0] >= second[0] and first[1] <= second[1]
+        ),
+        Corner.BOTTOM_RIGHT: (
+            lambda first, second: first[0] >= second[0] and first[1] >= second[1]
+        ),
+        Corner.BOTTOM_LEFT: (
+            lambda first, second: first[0] <= second[0] and first[1] >= second[1]
+        ),
+    }
+    for corner in corners:
+        position, type = corner
 
-    max_area = 0
-    for i in range(len(tiles)):
-        for j in range(i + 1, len(tiles)):
-            first, second = tiles[i], tiles[j]
-
-            min_x, max_x = ascending(first[0], second[0])
-            min_y, max_y = ascending(first[1], second[1])
-
-            is_rectangle = True
-            for x, y in red_tiles:
-                if min_x < x < max_x and min_y < y < max_y:
-                    is_rectangle = False
-            if not is_rectangle:
+        possible_rectangles = []
+        for second_corner in corners:
+            if corner == second_corner:
                 continue
-
-            third, fourth = (first[0], second[1]), (second[0], first[1])
-            if is_inside(valid_rectangles, third) and is_inside(
-                valid_rectangles, fourth
-            ):
-                area = (abs(first[0] - second[0]) + 1) * (abs(first[1] - second[1]) + 1)
-                max_area = max(max_area, area)
-
-    return max_area
+            if valid_corner[type](position, second_corner[0]):
+                possible_rectangles.append(second_corner[0])
+        print(position, type.name, possible_rectangles)
